b2ffa9141ef9dde44a156686aee535242488e173
prasad-bigdp <bigdp585@gmail.com>
2023-12-07 21:46:53 +0530

    update

--- ecommerce/src/app/app.component.ts
+++ ecommerce/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { Cart, CartItem } from './models/cart.model';
 import { CartService } from './services/cart.service';
+import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,10 @@ import { CartService } from './services/cart.service';
 })
 export class AppComponent implements OnInit {
   cart: Cart = { items: [] };
-
-  constructor(private cartService: CartService) {}
+  @ViewChild("abc", { static: true }) ab!:ElementRef;
+  constructor(private cartService: CartService) {
+    console.log(this.ab)
+  }
 
   ngOnInit() {
     this.cartService.cart.subscribe((_cart:any) => {
